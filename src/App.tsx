e43ff6303d6b84cfe26be08e5f089908216c486c
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-12-23 22:01:27 +0530

    Restarted project Due To Poor Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HOC } from "./constants/Components";
+import { HOC } from "./Constants/Components";
 import HomeScreen from "./screens/HomeScreen";
 const App: FC = () => {
   return (
--- src/constants/Components.tsx
+++ src/constants/Components.tsx
@@ -1,35 +1,10 @@
-import React, { FC, useState } from "react";
+import { Children, FC } from "react";
 import { BrowserRouter, Routes } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { LogoWrapper } from "../styles/Components.styled";
-import { BsCameraVideoFill } from "react-icons/bs";
+
 export const HOC: FC = ({ children }) => {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
-  const [accentColor, setAccentColor] = useState("#bd14ca");
-
   return (
-    <ThemeProvider
-      theme={{
-        theme,
-        accentColor,
-        setTheme,
-        setAccentColor,
-      }}
-    >
-      <BrowserRouter>
-        <Routes>{children}</Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
-};
-
-export const Logo: FC<{ size: number; style?: React.CSSProperties }> = ({
-  size,
-  style,
-}) => {
-  return (
-    <LogoWrapper style={{ ...style, height: size * 15, width: size * 15 }}>
-      <BsCameraVideoFill fontSize={size * 9} />
-    </LogoWrapper>
+    <BrowserRouter>
+      <Routes>{children}</Routes>
+    </BrowserRouter>
   );
 };
