8875504c657de435178b130280535e377a88a03a
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2022-01-19 11:03:01 +0530

    Refreshed project

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import { render } from "react-dom";
-import App from "./App";
-const root = document.getElementById("root");
-render(<App />, root);
+import { FC } from "react";
+import ReactDOM from "react-dom";
+const App: FC = () => {
+  return <h1>HELLLO I AM THE APP</h1>;
+};
+ReactDOM.render(<App />, document.getElementById("root"));
